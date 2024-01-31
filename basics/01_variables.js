09485f0ba863134debc1b93dce4bbb4636acb8fa
Ankita Saha <65508136+an861@users.noreply.github.com>
2024-01-31 12:40:17 +0000

    variables

---
+++ basics/01_variables.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Congratulations";
+console.log(greetings);
+var user_id = 346278;
+console.log(user_id);
+var islogged = true;
+console.log(islogged);
+islogged.valueOf();
---
+++ basics/01_variables.ts
@@ -0,0 +1,20 @@
+let greetings:string = "Congratulations"
+console.log(greetings)
+
+let user_id = 346278
+console.log(user_id)
+// TYPE INFERENCE means here TS is intelligent enough to determine the type of the variable
+// Do not 'ANY'
+
+let islogged:boolean = true
+console.log(islogged)
+
+let hero : string;
+
+function getHero() {
+    return "Thor"
+}
+
+hero = getHero()
+
+export{}
